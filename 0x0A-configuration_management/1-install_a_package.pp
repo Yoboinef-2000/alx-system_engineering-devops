415af61ea06ece00aaff59d66f08f05459cd41d7
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-04-19 22:49:53 +0300

    Then we need a verse for the swifties.

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 # This puppet manifest installs flask from pip3
-package { 'flask':
-  ensure   => '2.1.0',
-  name     => 'flask',
-  provider => 'pip3',
+exec { 'InstallingFlask':
+  command => 'pip3 install flask==2.1.0',
+  path    => ['/usr/bin/']
 }

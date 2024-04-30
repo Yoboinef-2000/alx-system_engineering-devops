4f134e4d1ee88af8b3a078588f6024d3e184bdaf
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-05-01 01:33:41 +0300

    Wee GOtta hold ooooon TO what we have gaaaat. it doesnt make a difference if we make it or not.

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,21 +1,8 @@
 # This puppet manifest configures a brand new Ubuntu machine
 
-exec { 'updatePackages':
-  command => '/usr/bin/apt-get update',
-}
-
-exec { 'installNGINX':
-  command => '/usr/bin/apt-get -y install nginx',
-}
-
-file_line {'changeX':
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'listen 80 default_server;',
-  line    => 'add_header X-Served-By $hostname;',
-  require => Package['nginx'],
-}
-
-exec { 'restartNGINX':
-  command => 'service nginx restart',
-  require => Exec['installNGINX'],
+exec { 'updateInstallANDrun':
+  command  => 'apt-get -y update; apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 }

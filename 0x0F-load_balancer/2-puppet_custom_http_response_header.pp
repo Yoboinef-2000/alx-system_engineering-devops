1a67b771809f33f02242915429a7f163a8b7e56f
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-05-01 01:08:32 +0300

    We GOtta hold ooooon TO what we have gaaaat. it doesnt make a difference if we make it or not.

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,10 +4,11 @@ exec { 'updateANDinstall':
   command => '/usr/bin/apt-get update && /usr/bin/apt-get -y install nginx',
 }
 file_line {'changeX' :
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'add_header X-Served-By $hostname;',
+  ensure  => present,
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'add_header X-Served-By $hostname;',
+  require => Package['nginx'],
 }
 service { 'nginx':
   ensure  => running,

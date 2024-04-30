9cce6007ff27b90ef7605d610ab4f200e5c85d2f
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-05-01 01:05:43 +0300

    We GOtta hold ooooon TO what we have gaaaat. it doesnt make a difference if we make it or not.

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,13 +3,7 @@
 exec { 'updateANDinstall':
   command => '/usr/bin/apt-get update && /usr/bin/apt-get -y install nginx',
 }
-file_line {'firstchange':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-}
-file_line {'secondchange' :
+file_line {'changeX' :
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',

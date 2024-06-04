b16a97ccf3f35251afd35d66e78d22f6f6bcf746
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-06-04 17:02:26 +0300

    We wont be gone for long.

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+QUery the Reddit API and return
+the number of subscirbers for a given
+subreddit.
+"""
+
+import requests
+# import sys
+
+
+def number_of_subscribers(subreddit):
+    """Refer to the commented out lines at the top."""
+
+    # theSubreddit = sys.argv[1]
+    theAPI = "https://www.reddit.com/r/"
+    userResponse = requests.get('{}{}/about.json'.format(theAPI, subreddit))
+    if userResponse.status_code == 200:
+        return (userResponse.json()['data']['subscribers'])
+    else:
+        return 0

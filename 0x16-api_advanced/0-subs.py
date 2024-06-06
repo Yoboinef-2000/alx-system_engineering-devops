5d9c7fd80c96d3af013237b3537ad24eb5e2239f
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-06-07 01:07:21 +0300

    Lets see if this works.

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,20 +13,9 @@ import requests
 def number_of_subscribers(subreddit):
     """Refer to the commented out lines at the top."""
 
-    # theSubreddit = sys.argv[1]
-    # theAPI = "https://www.reddit.com/r/"
-    # userResponse = requests.get('{}{}/about.json'.format(theAPI, subreddit),
-    #                             allow_redirects=False)
-
-    # if userResponse.status_code == 200:
-    #     return (userResponse.json()['data']['subscribers'])
-    
-    # else:
-    #     return 0
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0 (compatible; MyScript/1.0)'}
     
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, allow_redirects=False)
     
     if response.status_code == 200:
         data = response.json().get('data', {})

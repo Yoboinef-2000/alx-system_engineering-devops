3c05b72138c1e3a428285b81ec69400d0b87f53a
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-06-07 00:25:51 +0300

    Ola.

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,7 +2,7 @@
 
 """
 Query the Reddit API and print the titles
-of the first 10 hot postts listed for a given
+of the first 10 hot posts listed for a given
 subreddit.
 """
 
@@ -11,16 +11,26 @@ import requests
 
 def top_ten(subreddit):
     """Refer to the commented out lines above."""
-    here = "https://www.reddit.com/r/"
-    headThatThang = {'User-Agent': 'IDK_if_this_will_work'}
-    gerrrit = requests.get("{}{}/hot.json?limit=10".format(here, subreddit),
-                           allow_redirects=False,
-                           headers=headThatThang)
+    # here = "https://www.reddit.com/r/"
+    # headThatThang = {'User-Agent': 'Let_this_if_this_works'}
+    # gerrrit = requests.get("{}{}/hot.json?limit=9".format(here, subreddit),
+    #                        allow_redirects=False,
+    #                        headers=headThatThang)
 
-    if gerrrit.status_code == 200:
-        hotInHere = gerrrit.json().get('data', {}).get('children', [])
+    # if gerrrit.status_code == 200:
+    #     hotInHere = gerrrit.json().get('data', {}).get('children', [])
 
-        for aPost in hotInHere:
-            print(aPost['data']['title'])
-    else:
+    #     for aPost in hotInHere:
+    #         print(aPost['data']['title'])
+    # else:
+    #     print('None')
+    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
         print('None')
+    else:
+        [print(child.get("data").get("title"))
+         for child in sub_info.json().get("data").get("children")]
+

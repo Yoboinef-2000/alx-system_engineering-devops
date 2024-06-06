a84f8c87bf49b775820106d4bd76c759bdab09a8
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-06-07 01:05:06 +0300

    Lets see if this works.

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,26 +11,14 @@ import requests
 
 def top_ten(subreddit):
     """Refer to the commented out lines above."""
-    # here = "https://www.reddit.com/r/"
-    # headThatThang = {'User-Agent': 'Let_this_if_this_works'}
-    # gerrrit = requests.get("{}{}/hot.json?limit=9".format(here, subreddit),
-    #                        allow_redirects=False,
-    #                        headers=headThatThang)
+    here = "https://www.reddit.com/r/"
+    gerrrit = requests.get("{}{}/hot.json?limit=9".format(here, subreddit),
+                           allow_redirects=False)
 
-    # if gerrrit.status_code == 200:
-    #     hotInHere = gerrrit.json().get('data', {}).get('children', [])
+    if gerrrit.status_code == 200:
+        hotInHere = gerrrit.json().get('data', {}).get('children', [])
 
-    #     for aPost in hotInHere:
-    #         print(aPost['data']['title'])
-    # else:
-    #     print('None')
-    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
-        print('None')
+        for aPost in hotInHere:
+            print(aPost['data']['title'])
     else:
-        [print(child.get("data").get("title"))
-         for child in sub_info.json().get("data").get("children")]
-
+        print('None')

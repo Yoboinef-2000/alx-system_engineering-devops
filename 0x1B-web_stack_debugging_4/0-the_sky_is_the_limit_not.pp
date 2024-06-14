d1dbcec677fbd3783c02ed5b3f2b50367d318191
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-06-14 04:01:15 +0300

    Praise jah in the Moonlight.

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,16 +3,17 @@
 # of failed requests.
 
 # But the problem is my nginx server is not showing
-# any failed-request isssues so I am just what the potential
-# issue of the server in the exercise is supposd to be
+# any failed-request isssues so I am just going off
+# what the potential issue of the server in the exercise 
+# is supposd to be
 
 # When I went to nginx in /etc, everything was commmented out.
 # One of the comments was that ulimit was set to 4096.
 # So I am just going to play around with that and whatever sticks, sticks.
 
-file { '/etc/default/nginx':
-  ensure  => 'file',
-  content => "ULIMIT=\"-n 1048576\"\n",
+exec { 'letsSeeIfThisWorks':
+  command  => "sed -i 's/15/4096/' /etc/default/nginx",
+  provider => shell,
 }
 
 exec { 'reloooadTheThing':
